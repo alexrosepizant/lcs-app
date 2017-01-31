c5386b97a812f7cb85102cb97b657235d46b1876
alexrosepizant <alexandre.rosepizant@gmail.com>
2017-01-31 23:58:46 +0100

    Fix missing import

--- src/app/config/app.constants.js
+++ src/app/config/app.constants.js
@@ -1,7 +1,7 @@
 export default {
   appName: "Les coqs soccer",
   url: "localhost",
-  port: 3000,
+  port: 80,
   defaultImg: "https://placehold.it/128x128",
   aticlePerPage: 10,
   notificationCount: 15,
